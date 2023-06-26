5a1b3076e3bea272aba4a26ce3661d9d7911c40c
Leoodaviid <david_ax7@hotmail.com>
2023-06-26 08:36:20 -0300

    feat: adding SelectOrder component and adding filter functionality

---
+++ src/components/SelectOrder.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState } from "react";
+import { AiOutlineCaretUp } from "react-icons/ai";
+
+interface SelectProps {
+  options: { value: string; label: string }[];
+  onOrderChange?: (value: string) => void;
+  label?: string;
+}
+
+const SelectOrder = ({ options, onOrderChange, label }: SelectProps) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-full lg:w-[46%] flex justify-end items-center gap-3">
+      {label && <span className="font-bold">{label}</span>}
+      <div className="relative">
+        <select
+          onChange={(event) =>
+            onOrderChange && onOrderChange(event.target.value)
+          }
+          onClick={() => setOpen(!open)}
+          onBlur={() => setOpen(false)}
+          className="w-48 border border-zinc-900 rounded-xl appearance-none pl-3 py-1 font-medium"
+        >
+          {options.map(({ value, label }) => (
+            <option key={value} value={value}>
+              {label}
+            </option>
+          ))}
+        </select>
+        <div className="absolute top-0.5 right-3 h-full flex items-center pointer-events-none rotate-180">
+          <AiOutlineCaretUp
+            className={`${open && "rotate-180"} h-5 w-5`}
+            aria-hidden="true"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SelectOrder;
--- src/components/WebinarsList.tsx
+++ src/components/WebinarsList.tsx
@@ -1,49 +1,64 @@
 "use client";
 import React, { useState } from "react";
 import WebinarsCard from "@/components/WebinarsCard";
+import Filter from "@/components/Filter";
+import SelectOrder from "@/components/SelectOrder";
 import Pagination from "@/components/Pagination";
 import { useWebinars, useAllWebinars } from "@/hooks/useWebinars";
-import { Filter } from "@/components/Filter";
 import { getCategoryFrom } from "@/utils/webinarsUtils";
 
 const WebinarsList = () => {
-  const [currentCategory, setCurrentCategory] = useState<string>();
+  const [currentCategory, setCurrentCategory] = useState<string | undefined>(
+    "Agências"
+  );
+  const [currentOrder, setCurrentOrder] = useState<string>("asc");
   const {
     data: webinars,
     currentPage,
     setCurrentPage,
     totalPages,
-  } = useWebinars(currentCategory);
+  } = useWebinars(currentCategory, currentOrder);
   const { data: allWebinarsData } = useAllWebinars();
   const webinarsCategory = getCategoryFrom(allWebinarsData);
 
   const onFilterClick = (category?: string) => {
     if (category === currentCategory) {
-      setCurrentCategory(undefined);
+      setCurrentCategory("");
     } else {
       setCurrentCategory(category);
     }
   };
 
   return (
-    <section>
-      <Filter
-        category={webinarsCategory}
-        currentCategory={currentCategory}
-        onClick={onFilterClick}
-      />
-
-      <div className="w-full max-w-[1024px] m-auto grid grid-cols-auto sm:grid-cols-2 md:grid-cols-3 justify-center items-center gap-5 py-10 px-5 border-b border-slate-300 mb-10">
-        {webinars?.map((webinar) => (
-          <WebinarsCard key={webinar.id} data={webinar} />
-        ))}
-      </div>
-      <div className="flex items-center justify-center my-5">
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={(page) => setCurrentPage(page)}
-        />
+    <section className="py-12">
+      <div className="container">
+        <div className="w-full max-w-[1320px] m-auto flex flex-col gap-3 justify-center lg:flex-row items-center py-5 border-b border-slate-300">
+          <Filter
+            category={webinarsCategory}
+            currentCategory={currentCategory}
+            onClick={onFilterClick}
+          />
+          <SelectOrder
+            options={[
+              { value: "asc", label: "Data de Publicação" },
+              { value: "desc", label: "Mais Recentes" },
+            ]}
+            onOrderChange={setCurrentOrder}
+            label="Ordenar por"
+          />
+        </div>
+        <div className="w-full max-w-[1320px] m-auto grid grid-cols-auto sm:grid-cols-2 md:grid-cols-3 justify-center items-center gap-5 py-12 border-b border-slate-300">
+          {webinars?.map((webinar) => (
+            <WebinarsCard key={webinar.id} data={webinar} />
+          ))}
+        </div>
+        <div className="flex items-center justify-center py-5">
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={(page) => setCurrentPage(page)}
+          />
+        </div>
       </div>
     </section>
   );

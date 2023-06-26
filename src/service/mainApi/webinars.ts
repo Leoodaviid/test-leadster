d7eb4cb5964ffdbda4dbd19c1e6f999b1bd00d22
Leoodaviid <david_ax7@hotmail.com>
2023-06-26 08:33:51 -0300

    feat : add order parameter and update API call

--- src/service/mainApi/webinars.ts
+++ src/service/mainApi/webinars.ts
@@ -9,9 +9,10 @@ import { WEBINARS_PER_PAGE } from "@/utils/webinarsUtils";
 export const getWebinars = async ({
   page,
   category,
+  order,
 }: GetWebinarsProps): Promise<GetWebinarsResponse> => {
   const response = await api.get<WebinarsProps[]>(
-    `/webinars?_page=${page}&_limit=${WEBINARS_PER_PAGE}&category=${category}`
+    `/webinars?_page=${page}&_limit=${WEBINARS_PER_PAGE}&category=${category}&_sort=createdAt&_order=${order}`
   );
 
   const total = parseInt(response.headers["x-total-count"]);
@@ -19,5 +20,4 @@ export const getWebinars = async ({
   return { data: response.data, total };
 };
 
-export const getAllWebinars = () =>
-  api.get<WebinarsProps[]>(`/webinars?_sort=category&_order=asc`);
+export const getAllWebinars = () => api.get<WebinarsProps[]>(`/webinars`);

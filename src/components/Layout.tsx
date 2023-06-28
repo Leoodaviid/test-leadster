f354ad713b988fdbcff441f7cb60fcdf389a33d4
Leoodaviid <david_ax7@hotmail.com>
2023-06-28 01:41:07 -0300

    feat: adding WebinarsModal to Layout for global use and openModal in WebinarsCard

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,23 @@
+"use client";
 import React from "react";
 import Header from "./Header";
 import { ReactNode } from "react";
+import WebinarsModal from "@/components/WebinarsModal";
+import useModal from "@/hooks/useModal";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const { isOpen, closeModal } = useModal();
   return (
     <>
       <Header />
-      <main className="w-full m-auto">{children}</main>
+      <main className="w-full m-auto">
+        <WebinarsModal visible={isOpen} onClose={closeModal} />
+        {children}
+      </main>
     </>
   );
 };
--- src/components/WebinarsCard.tsx
+++ src/components/WebinarsCard.tsx
@@ -4,23 +4,28 @@ import cardImage from "/public/thumbnail.png";
 import { useRouter } from "next/navigation";
 import { WebinarsProps } from "@/models/webinars";
 import { FaPlay } from "react-icons/fa";
+import useModal from "@/hooks/useModal";
 
 interface WebinarsCardProps {
   data: WebinarsProps;
 }
 
 const WebinarsCard = ({ data }: WebinarsCardProps) => {
+  const { openModal } = useModal();
   const router = useRouter();
   return (
     <div className="text-zinc-700 cursor-pointer rounded-2xl shadow-xl bg-white group">
-      <div className="rounded-t-2xl group-hover:bg-[#007eff] relative transition">
+      <div
+        className="rounded-t-2xl group-hover:bg-[#007eff] relative transition"
+        onClick={() => openModal(data.id)}
+      >
         <Image
           src={cardImage}
           width={500}
           height={500}
           alt={data.title}
           className="group-hover:opacity-70"
-          onClick={() => router.push(`/webinars?id=${data.id}`)}
+          // onClick={() => router.push(`/webinars?id=${data.id}`)}
         />
         <FaPlay
           className="hidden group-hover:flex absolute top-[37.5%] left-[42%] drop-shadow-2xl transition"

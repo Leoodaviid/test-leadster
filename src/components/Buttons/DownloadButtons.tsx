e64b23a1cffb3082f2d41b7a9620c2ed1ed026bb
Leoodaviid <david_ax7@hotmail.com>
2023-06-28 01:36:28 -0300

    feat: video component and buttons downloads component

---
+++ src/components/Buttons/DownloadButtons.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { BsCloudDownload } from "react-icons/bs";
+
+interface DownloadButtonsProps {
+  onDownload: (fileName: string) => void;
+}
+
+const DownloadButtons = ({ onDownload }: DownloadButtonsProps) => {
+  return (
+    <>
+      <div className="flex justify-center items-center text-[#00ae7f]">
+        <BsCloudDownload
+          size={16}
+          className="bg-[#9fefdf] text-green-600 h-7 w-7 pt-1 pb-0.5 px-1.5 rounded-l-md font-semibold"
+        />
+        <button
+          className="flex justify-center items-center gap-1 bg-[#c2f4ea] px-2 py-1.5 rounded-r-md text-xs font-semibold"
+          onClick={() => onDownload("Spreadsheet.xls")}
+        >
+          Spreadsheet.xls
+        </button>
+      </div>
+      <div className="flex justify-center items-center text-[#007eed]">
+        <BsCloudDownload
+          size={16}
+          className="bg-[#a1d9ff] text-[#007eed] h-7 w-7 pt-1 pb-0.5 px-1.5 rounded-l-md font-semibold"
+        />
+        <button
+          className="flex justify-center items-center gap-1 bg-[#c2e6ff] px-2 py-1.5 rounded-r-md text-xs font-semibold"
+          onClick={() => onDownload("Spreadsheet.xls")}
+        >
+          Document.doc
+        </button>
+      </div>
+      <div className="flex justify-center items-center text-[#b7a35f]">
+        <BsCloudDownload
+          size={16}
+          className="bg-[#fff1a0] text-[#b7a35f] h-7 w-7 pt-1 pb-0.5 px-1.5 rounded-l-md font-semibold"
+        />
+        <button
+          className="flex justify-center items-center gap-1 bg-[#fff8d0] px-2 py-1.5 rounded-r-md text-xs font-semibold"
+          onClick={() => onDownload("Spreadsheet.xls")}
+        >
+          Presentation.ppt
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default DownloadButtons;
---
+++ src/components/Video.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import { FaPlay } from "react-icons/fa";
+
+interface VideoProps {
+  url: string;
+  thumbnail?: string;
+  width: number;
+  height: number;
+}
+
+const Video = ({ url, thumbnail, width, height }: VideoProps) => {
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  const handlePlayClick = () => {
+    setIsPlaying(true);
+  };
+
+  return (
+    <>
+      <ReactPlayer
+        controls={true}
+        url={url}
+        width={width}
+        height={height}
+        style={{ opacity: isPlaying ? 1 : 0 }}
+        playing={isPlaying}
+      />
+      {!isPlaying && (
+        <>
+          <img
+            src={thumbnail}
+            width={width}
+            height={height}
+            className="absolute top-0 left-0"
+          />
+          <button
+            onClick={handlePlayClick}
+            className="absolute group-hover:flex top-[52%] left-[51%] drop-shadow-2xl transition transform -translate-x-1/2 -translate-y-1/2 text-6xl bg-transparent border-none cursor-pointer"
+          >
+            <FaPlay className="" size={95} color="white" />
+          </button>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Video;
